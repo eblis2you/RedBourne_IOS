bbb5c920795d3f4731c23e21699e639a80507678
Jerry Wu <Jerry@Jerrys-Mac.local>
2013-09-13 13:11:26 +1000

    Initial Commit

---
+++ RedBourne_IOS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RedBourne_IOS
+//
+//  Created by Jerry on 13/09/13.
+//  Copyright (c) 2013 Qodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RedBourne_IOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RedBourne_IOS
+//
+//  Created by Jerry on 13/09/13.
+//  Copyright (c) 2013 Qodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
